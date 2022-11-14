871b5c39dbaf9374a30f2505701bfc0ecae44dc1
Francisco <Francisco@DESKTOP-KD6DH0L>
2022-11-14 22:01:44 +0100

    Agregar archivos de proyecto.

---
+++ CsPharma-V4/Program.cs
@@ -0,0 +1,36 @@
+using DAL;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddEntityFrameworkNpgsql()
+    .AddDbContext<CsPharmaV4Context>(options =>
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString("EFCConexion"));
+    });
+
+var app = builder.Build();
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
